bd179ad76768551ec7ef6d433cb7e1e6ab934399
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2022-04-22 17:24:52 +0530

    Create initializationArray.cpp

---
+++ C++ programming/STL/Array/initializationArray.cpp
@@ -0,0 +1,9 @@
+#include <array>
+int main()
+{
+    //It will create an empty integer array of size 3 
+    array<int, 3> numbers;
+    //It will create an integer array of size 3 having elements 1,2 & 3
+    array<int, 3> num = {1, 2, 3};
+    return 0;
+}
