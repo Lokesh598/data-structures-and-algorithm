fb57de5d0ce7a63aee7d5c717486fa747be74103
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2022-05-01 09:34:09 +0530

    Update 1. printString.cpp

--- String/1. printString.cpp
+++ String/1. printString.cpp
@@ -14,3 +14,22 @@ int main() {
     cout << str << endl;
     return 0;
 }
+
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+void printString(string str, int n) {
+    for(int i = 0; i < n; i++) {
+        cout << str[i] ;
+    }
+    cout<<endl;
+}
+int main() {
+    string str = "hello world";
+    int n = str.length();
+    printString(str, n);
+    return 0;
+}
