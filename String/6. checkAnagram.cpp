ffc49fd75b6d25ab3bcf3d9365651a35f31615d0
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2022-05-01 15:55:09 +0530

    Create 6. checkAnagram.cpp

---
+++ String/6. checkAnagram.cpp
@@ -0,0 +1,44 @@
+// check anagram or not with auxilary space
+
+//input: two strings
+//output: true or false
+//example:
+//"abc" "cba"
+//"abc" "cbaa"
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+const int CHAR=256;
+bool areAnagram(string &s1, string &s2) 
+{ 
+    int n1 = s1.length(); 
+    int n2 = s2.length(); 
+    if (n1 != n2) 
+        return false; 
+    
+    int count[CHAR]={0};
+    for(int i=0;i<s1.length();i++){
+        count[s1[i]]++;
+        count[s2[i]]--;
+    }
+    
+    for(int i=0;i<CHAR;i++){
+        if(count[i]!=0)return false;
+    }
+    return true;
+} 
+ 
+int main() 
+{ 
+    string str1 = "abaac"; 
+    string str2 = "aacba"; 
+    if (areAnagram(str1, str2)) 
+        cout << "The two strings are anagram of each other"; 
+    else
+        cout << "The two strings are not anagram of each other"; 
+  
+    return 0; 
+} 
