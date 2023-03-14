c6565721a863c00959bd4ef3ea9ed82c03891fd7
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2023-03-15 00:47:49 +0530

    Create

---
+++ LinkedList/6. detect loop in linkedlist.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+    public:
+    //Function to check if the linked list has a loop.
+    bool detectLoop(Node* head)
+    {
+        if(head == NULL or head->next == NULL)
+            return false;
+        // your code here
+        Node *slow = head;
+        Node *fast = head;
+        while(fast->next != NULL and fast->next->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast)
+                return 1;
+        }
+        return 0;
+    }
+};
