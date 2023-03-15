be47896392feb364e4cd61800321528c579c694f
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2023-03-16 00:21:59 +0530

    Create

---
+++ LinkedList/8. rotatealinkedlist.cpp
@@ -0,0 +1,42 @@
+class Solution
+{
+    public:
+    //Function to rotate a linked list.
+    Node* rotate(Node* head, int k)
+    {
+        // Your code here
+        // Node *cur = head;
+        // Node *tmp = head;
+        
+        // for(int i = 0; i<k-1; i++) {
+        //     tmp = tmp->next;
+        // }
+        // while(cur->next!=NULL) {
+        //     cur = cur->next;
+        // }
+        // cur->next = head;
+        // head = tmp->next;
+        // tmp->next = NULL;
+        // return head;
+        
+        Node *walk = head;
+        Node *prev;
+        
+        while(k-- && walk!=NULL) {
+            prev = walk;
+            walk = walk->next;
+        }
+        
+        if(!walk) return head;
+        
+        Node *newHead = walk;
+        prev->next = NULL;
+        while(walk->next) {
+            walk = walk->next;
+        }
+        walk->next = head;
+        
+        
+        return newHead;
+    }
+};
