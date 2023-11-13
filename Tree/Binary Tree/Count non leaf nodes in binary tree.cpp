6610e8884a69605b9bf7ce07593bcc252d201337
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2023-11-14 00:18:19 +0530

    Create Count non leaf nodes in binary tree.cpp

---
+++ Tree/Binary Tree/Count non leaf nodes in binary tree.cpp
@@ -0,0 +1,14 @@
+count non leaf nodes in binary tree
+
+
+int countNonLeafNodes(TreeNode* root) {       
+        if (root == NULL)
+            return 0;
+        if (root->left == null && root->right == null) {
+            return 0;
+        }
+        int l = countNodes(root->left);
+        int r = countNodes(root->right);
+
+        return 1 + l + r;       
+}
