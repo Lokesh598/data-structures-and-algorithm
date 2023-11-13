158fe06a2af6eabc0dfc2914cac7d5e945401129
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2023-11-14 00:16:46 +0530

    Update Total nodes in complete binary tree.cpp

--- Tree/Binary Tree/Total nodes in complete binary tree.cpp
+++ Tree/Binary Tree/Total nodes in complete binary tree.cpp
@@ -11,3 +11,19 @@ public:
         return 1 + l + r;
     }
 };
+
+// count non leaf nodes in binary tree
+
+
+int countNonLeafNodes(TreeNode* root) {
+        
+        if (root == NULL)
+            return 0;
+        if (root->left == null && root->right == null) {
+            return 0;
+        }
+        int l = countNodes(root->left);
+        int r = countNodes(root->right);
+
+        return 1 + l + r;
+    }
