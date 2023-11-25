28572b59a8938bb587955a962a1d94eb2cf563e1
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2023-11-26 00:36:57 +0530

    Create Insert Intervals.cpp

---
+++ Array/Insert Intervals.cpp
@@ -0,0 +1,26 @@
+next problem of merge intervals
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+
+        intervals.push_back(newInterval);
+
+        if(intervals.size()<=1) return intervals;
+
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> output;
+
+        output.push_back(intervals[0]);
+
+        for(int i = 1; i < intervals.size(); i++) {
+            if(output.back()[1] >= intervals[i][0]) 
+                output.back()[1] = max(output.back()[1], intervals[i][1]);
+
+            else output.push_back(intervals[i]);
+        }
+        return output;
+    }
+};
