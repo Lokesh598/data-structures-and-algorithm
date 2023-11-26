262864878e658cd0baa59d77abfeffb4025fe203
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2023-11-26 22:10:56 +0530

    Create String Sort.cpp

---
+++ String/String Sort.cpp
@@ -0,0 +1,11 @@
+string strSort(string s) {
+        int counter[26] = {0};
+        for (char c : s) {
+            counter[c - 'a']++;
+        }
+        string t;
+        for (int c = 0; c < 26; c++) {
+            t += string(counter[c], c + 'a');
+        }
+        return t;
+    }
