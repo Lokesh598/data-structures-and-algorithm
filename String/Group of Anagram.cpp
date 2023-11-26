b0f867ad860aa4b599135d666f035098cef288d3
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2023-11-26 22:12:06 +0530

    Create Group of Anagram.cpp

---
+++ String/Group of Anagram.cpp
@@ -0,0 +1,27 @@
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        unordered_map<string, vector<string>> mp;
+
+        vector<vector<string>> res;
+
+        for (auto x: strs) {
+            string word = x;
+
+            sort(word.begin(), word.end());
+
+            mp[word].push_back(x);
+        }
+
+        for (auto str : mp) {
+            res.push_back(str.second);
+        }
+        return res;
+    }
+};
