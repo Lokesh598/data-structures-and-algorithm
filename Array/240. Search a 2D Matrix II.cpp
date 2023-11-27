8f5ce245d981b7092a6f982b51c33f2b0a1dc45e
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2023-11-27 23:41:42 +0530

    Create 240. Search a 2D Matrix II.cpp

---
+++ Array/240. Search a 2D Matrix II.cpp
@@ -0,0 +1,17 @@
+1. linear search or brute force solution
+
+```
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        //linear search
+        int m = matrix.size();
+        int n = matrix[0].size();
+        for(int i = 0; i<m; i++) {
+            for(int j = 0; j<n; j++) {
+                if(matrix[i][j] == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+```
