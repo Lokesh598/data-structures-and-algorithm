1ca09415e66328b2b71d5ab5250c3ee4a99761c3
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2023-11-29 23:59:05 +0530

    Update 240. Search a 2D Matrix II.cpp

--- Array/240. Search a 2D Matrix II.cpp
+++ Array/240. Search a 2D Matrix II.cpp
@@ -17,3 +17,36 @@ bool searchMatrix(vector<vector<int>>& matrix, int target) {
 ```
 
 2. efficient, using binary search 
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+		// if matrix have 0 rows or 0 colums
+        if(n == 0 || m == 0)
+            return false;
+        
+		// treating matrix as array just taking care of endices
+		// [0..n*m]
+        int start = 0, end = m*n - 1;
+        
+        while(start <= end)
+        {
+            int mid = start + (end - start) / 2;
+			// a[x] : matrix[x / m][x % m] formulae
+            int ind = matrix[mid/m][mid%m];
+            if (target == ind)
+                return true;
+			// left half
+            else if(target < ind)
+                end = mid - 1;
+            else
+			// right half
+                start = mid + 1;       
+        }
+        return false;
+   
+    }
+};
