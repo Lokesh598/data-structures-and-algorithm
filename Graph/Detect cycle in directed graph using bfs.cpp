899164711e718d42c97afcb2bf640c57bba0f5cd
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2023-12-03 01:22:32 +0530

    Create Detect cycle in directed graph using bfs.cpp

---
+++ Graph/Detect cycle in directed graph using bfs.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    // Function to detect cycle in a directed graph.
+    bool isCyclic(int V, vector<int> adj[]) {
+        // code here
+        vector<int> res;
+	    vector<int> indegree(V, 0);
+	    
+	    for(int i = 0; i < V; i++) {
+	        for (int x : adj[i]) {
+	            indegree[x]++;
+	        }
+	    }
+	    queue<int> q;
+	    
+	    for(int i = 0; i < V; i++) {
+	        if(indegree[i] == 0)
+	            q.push(i);
+	    }
+	    int count = 0;
+	    while(!q.empty()) {
+	        int x = q.front();
+	        q.pop();
+	        
+	        res.push_back(x);
+	        for(int i : adj[x])
+	            if(--indegree[i] == 0)
+	                q.push(i);
+	                
+	        count++;
+	    }
+	    return (count != V);
+    }
+};
