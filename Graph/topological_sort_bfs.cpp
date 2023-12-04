fdb634887b802ed7c50282ceb8addc01f33ff076
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2023-12-04 21:58:13 +0530

    Create topological_sort_bfs.cpp

---
+++ Graph/topological_sort_bfs.cpp
@@ -0,0 +1,25 @@
+void topologicalSort(vector<int> adj[], int V) 
+{ 
+    vector<int> in_degree(V, 0); 
+  
+    for (int u = 0; u < V; u++) { 
+        for (int x:adj[u]) 
+            in_degree[x]++; 
+    } 
+  
+    queue<int> q; 
+    for (int i = 0; i < V; i++) 
+        if (in_degree[i] == 0) 
+            q.push(i); 
+
+  
+    while (!q.empty()) { 
+        int u = q.front(); 
+        q.pop(); 
+        cout<<u<<" "; 
+  
+        for (int x: adj[u]) 
+            if (--in_degree[x] == 0) 
+                q.push(x); 
+    } 
+}
