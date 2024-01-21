22713479d69205b5de989c666038bbc45d7de8b0
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2024-01-21 21:44:27 +0530

    Create House Robber.java

---
+++ Dynamic Programming/House Robber.java
@@ -0,0 +1,20 @@
+leetcode 198 problem
+
+class Solution {
+    static int[] dp;
+    public int rob(int[] nums) {
+        dp = new int[nums.length];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i] = -1;
+        }
+        return robHelper(nums, nums.length-1);
+    }
+
+    private static int robHelper(int[] nums, int n) {
+        if (n < 0) return 0;
+        if (dp[n] != -1) return dp[n];
+        dp[n] = Math.max(robHelper(nums, n-2) + nums[n], robHelper(nums, n-1));
+
+        return dp[n];
+    }
+}  
