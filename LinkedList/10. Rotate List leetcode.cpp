9a6291bd857f858d142b2b300b237a56c4c2307d
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2024-01-22 16:57:02 +0530

    Create 10. Rotate List leetcode.cpp

---
+++ LinkedList/10. Rotate List leetcode.cpp
@@ -0,0 +1,36 @@
+you just have to calculate k%n rotations ,where is n is number of nodes.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (!head) return head;
+
+        int len = 1;
+        ListNode *newH = head, *tail = head;
+
+        while (tail->next) {
+            tail = tail->next;
+            len++;
+        }
+        tail->next = head;
+
+        if (k = k%len) {
+            for (auto i = 0; i < len - k; i++){
+                tail = tail->next;
+            }
+        }
+        newH = tail->next;
+        tail->next = nullptr;
+        return newH;
+    }
+};
