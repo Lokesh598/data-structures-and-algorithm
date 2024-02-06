246117d877c119fe91de8093e29cc312956695aa
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2024-02-06 22:24:17 +0530

    Update 86. Partition List.cpp

--- LinkedList/86. Partition List.cpp
+++ LinkedList/86. Partition List.cpp
@@ -20,3 +20,36 @@ public:
         return left->next;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode left = new ListNode(0), right = new ListNode(0);
+        ListNode ltail = left, rtail = right;
+
+        while(head!=null) {
+            if(head.val < x) {
+                ltail.next = head;
+                ltail = ltail.next;
+            } else {
+                rtail.next = head;
+                rtail = rtail.next;
+            }
+            head = head.next;
+        }
+        ltail.next = right.next;
+        rtail.next = null;
+
+        return left.next;
+    }
+}
+
