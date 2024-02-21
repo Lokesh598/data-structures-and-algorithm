066c4d56817418f93d3a0cc0035291f88e87c51c
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2024-02-21 17:08:56 +0530

    tabulation solution

---
+++ Dynamic Programming/✨Wildcard Matching.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.length(), n = p.length();
+        bool dp[m+1][n+1];
+        memset(dp, false, sizeof(dp));
+        dp[0][0] = true;
+
+        for (int j = 1; j <= n; ++j) {
+            if (p[j - 1] == '*') {
+                dp[0][j] = dp[0][j-1];
+            }
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p[j - 1] == s[i - 1] || p[j - 1] == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                } else dp[i][j] = 0;
+            }
+        }
+        return dp[m][n];
+    }
+};
