f9990fed3bee91b5a0419c6ff3b8b253cdc4fda1
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2024-06-05 19:52:54 +0530

    Create Intersection of Two Arrays II.java

---
+++ Array/Hash Table/Intersection of Two Arrays II.java
@@ -0,0 +1,26 @@
+
+public class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        List<Integer> result = new ArrayList<>();
+        
+        for (int num : nums2) {
+            if (map.getOrDefault(num, 0) > 0) {
+                result.add(num);
+                map.put(num, map.get(num) - 1);
+            }
+        }
+        
+        int[] ans = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            ans[i] = result.get(i);
+        }
+        
+        return ans;
+    }
+}
